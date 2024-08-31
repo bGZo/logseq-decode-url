dd785e766fa4c60d3b884d49ea0fa76110a00610
菜就多練練 <im@bgzo.cc>
2024-08-31 10:13:04 +0800

    style: remove comment debug

--- index.js
+++ index.js
@@ -2,18 +2,8 @@ function main () {
   logseq.Editor.registerSlashCommand(
     'decode-url',
     async () => {
-      // const handle = [
-      //   '%20'
-      // ]
       const { content, uuid } = await logseq.Editor.getCurrentBlock()
       const decodedContent = decodeURI(content);
-
-      // var flag = false;
-      // handle.forEach(i => {
-      //   if (content.includes(i)) {
-      //     flag = true
-      //   }
-      // })
 
       if (content === decodedContent){
         logseq.App.showMsg(`
@@ -23,21 +13,9 @@ function main () {
         logseq.Editor.updateBlock(uuid, decodedContent )
       }
 
-      // logseq.App.showMsg(`
-      //   [:div.p-2
-      //     [:h1 "#${uuid}"]
-      //     [:h2.text-xl "${content}"]]
-      //     [:h2.text-xl "${decodedURL}"]]
-      // `)
     },
   )
 
-  // logseq.Editor.registerBlockContextMenuItem('🦜 Send A Tweet',
-  //   ({ blockId }) => {
-  //     logseq.App.showMsg(
-  //       '🦜 Tweet from block content #' + blockId,
-  //     )
-  //   })
 }
 
 // bootstrap
